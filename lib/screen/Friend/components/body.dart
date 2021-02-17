c6c000548dc9a4bcee69d5c73436e17985a4cb2c
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-02-17 17:19:30 +0900

    feat: freind part 구현

--- lib/screen/Friend/components/body.dart
+++ lib/screen/Friend/components/body.dart
@@ -18,21 +18,42 @@ class _BodyState extends State<Body> {
     print("이것은 $id");
   }
 
+  List user = [
+    "고병찬",
+    "김병찬",
+    "강병찬",
+    "공병찬",
+    "가병찬",
+    "길병찬",
+    "구병찬",
+    "기병찬",
+    "긴병찬",
+    "짧은병찬",
+    "홍병찬"
+  ];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int i) {
-            return ListTile(
-              leading: Icon(Icons.people),
-              title: Text("$id의친구${i + 1}"),
-              trailing: Icon(Icons.ac_unit),
-              onTap: () {},
-            );
-          }),
-    );
+        child: (user.length == 0)
+            ? Text("친구가 없어")
+            : ListView.separated(
+                physics: BouncingScrollPhysics(),
+                itemCount: user.length,
+                itemBuilder: (BuildContext context, int i) {
+                  return ListTile(
+                    leading: Icon(Icons.emoji_people),
+                    title: Text("${user[i]}"),
+                    trailing: IconButton(
+                      icon: Icon(Icons.arrow_left_outlined),
+                      color: Colors.black,
+                      onPressed: () {},
+                    ),
+                    onTap: () {},
+                  );
+                },
+                separatorBuilder: (BuildContext context, int i) {
+                  return const Divider();
+                }));
   }
 }
