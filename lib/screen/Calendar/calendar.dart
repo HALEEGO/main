95e167d924338e46bac99a350fa6066b0a3a6965
2sssg <lsg020300@naver.com>
2021-02-18 04:07:37 +0900

    REFACTOR calendarmain
    캘린더 메인부분 코드객체화

--- lib/screen/Calendar/calendar.dart
+++ lib/screen/Calendar/calendar.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-import 'dart:ui';
-
+import 'package:calendar/screen/Calendar/tablecalendar.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:table_calendar/table_calendar.dart';
-import '../Main/mainmenu.dart';
+import './Function/APIget.dart';
 
 class Calendar extends StatefulWidget {
   Calendar({Key key, this.title}) : super(key: key);
@@ -15,102 +12,14 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> with TickerProviderStateMixin {
-  String id;
-  List<dynamic> calendarList;
-  var calendarTYPEList = List();
-  final String URL = "http://3.35.39.202:8000/calendar";
-  final Map<DateTime, List> _holidays = {
-    DateTime(2021, 2, 1): [
-      'New Year\'s Day',
-      'New Year\'s Day',
-      'New Year\'s Day'
-    ],
-    DateTime(2021, 2, 6): ['Epiphany', 'Epiphany'],
-    DateTime(2021, 2, 14): [
-      'Valentine\'s Day',
-      'Valentine\'s Day',
-      'Valentine\'s Day'
-    ],
-    DateTime(2021, 2, 21): ['Easter Sunday'],
-    DateTime(2021, 2, 22): [
-      'Easter Monday',
-      'Easter Monday',
-      'Easter Monday',
-      'Easter Monday',
-      'Easter Monday'
-    ],
-  };
-  var _eventss = Map<DateTime, List>();
-  Map<DateTime, List> _events;
-  List _selectedEvents;
+  String id; //user의 id
   AnimationController _animationController;
   CalendarController _calendarController;
 
   @override
   void initState() {
     super.initState();
-    id = widget.title;
-    searchcalendar();
-    final _selectedDay = DateTime.now();
-
-    _events = {
-      _selectedDay.subtract(Duration(days: 30)): [
-        'Event A0',
-        'Event B0',
-        'Event C0'
-      ],
-      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
-      _selectedDay.subtract(Duration(days: 20)): [
-        'Event A2',
-        'Event B2',
-        'Event C2',
-        'Event D2'
-      ],
-      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
-      _selectedDay.subtract(Duration(days: 10)): [
-        'Event A4',
-        'Event B4',
-        'Event C4'
-      ],
-      _selectedDay.subtract(Duration(days: 4)): [
-        'Event A5',
-        'Event B5',
-        'Event C5'
-      ],
-      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
-      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
-      _selectedDay.add(Duration(days: 2)): [
-        'Event A8',
-        'Event B8',
-        'Event C8',
-        'Event D8'
-      ],
-      _selectedDay.add(Duration(days: 3)):
-          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
-      _selectedDay.add(Duration(days: 7)): [
-        'Event A10',
-        'Event B10',
-        'Event C10'
-      ],
-      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
-      _selectedDay.add(Duration(days: 17)): [
-        'Event A12',
-        'Event B12',
-        'Event C12',
-        'Event D12'
-      ],
-      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
-      _selectedDay.add(Duration(days: 26)): [
-        'Event A14',
-        'Event B14',
-        'Event C14'
-      ],
-    };
-    _eventss = {
-      DateTime(2021, 2, 21): ['events a14']
-    };
-
-    _selectedEvents = _events[_selectedDay] ?? [];
+    id = widget.title; //id 지역변수로 넣기
     _calendarController = CalendarController();
 
     _animationController = AnimationController(
@@ -128,342 +37,29 @@ class _CalendarState extends State<Calendar> with TickerProviderStateMixin {
     super.dispose();
   }
 
-  void searchcalendar() async {
-    Response response = await get("$URL/read/usercalendar/$id");
-    print("1");
-    calendarList = jsonDecode(response.body);
-    print("2");
-    print(calendarList[0]["calendarNUM"]);
-    for (int i = 0; i < calendarList.length; i++) {
-      calendarTYPEList.add(calendarList[i]["scheduleTYPE"]);
-      DateTime dt =
-          DateTime.parse("${calendarList[i]["scheduleDATE"]} 00:00:00.000");
-      if (_eventss.containsKey(dt)) {
-        _eventss[dt].add(calendarList[i]["scheduleTYPE"]);
-        _eventss[dt].add(calendarList[i]["calendarNUM"]);
-      } else {
-        _eventss[dt] = [calendarList[i]["scheduleTYPE"]];
-        _eventss[dt].add(calendarList[i]["calendarNUM"]);
-      }
-      // if(_eventss.containsKey(key))
-
-      // print("이름 : $_eventss");
-
-    }
-    print(_eventss);
-  }
-
-  void _onDaySelected(DateTime day, List events, List holidays) {
-    print('CALLBACK: _onDaySelected');
-    print("$day");
-    showMenu(day);
-  }
-
-  void _onVisibleDaysChanged(
-      DateTime first, DateTime last, CalendarFormat format) {
-    print('CALLBACK: _onVisibleDaysChanged');
-  }
-
-  void _onCalendarCreated(
-      DateTime first, DateTime last, CalendarFormat format) {
-    print('CALLBACK: _onCalendarCreated');
-  }
-
+//////////////////////////////////////메인 빌드////////////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          _buildTableCalendarWithBuilders(),
+          FutureBuilder(
+            future: searchcalendar(id),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return buildTableCalendarWithBuilders(
+                    _calendarController, _animationController, context);
+              } else {
+                return Container();
+              }
+            },
+          )
         ],
       ),
     );
   }
 
-  showMenu(DateTime day) {
-    bool tmp = _eventss.containsKey(DateTime.parse(
-        day.toString().replaceAll("12", "00").replaceAll("Z", "")));
-    int temp;
-    if (tmp) {
-      temp = _eventss[DateTime.parse(
-              day.toString().replaceAll("12", "00").replaceAll("Z", ""))]
-          .length;
-    } else {
-      temp = 0;
-    }
+////////////////////////////////////////메인 빌드/////////////////////////////////////////////
 
-    showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16.0),
-                topRight: Radius.circular(16.0),
-              ),
-              color: Color(0xff232f34),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Container(
-                  height: 36,
-                ),
-                SizedBox(
-                    height: (45 * 6).toDouble(),
-                    child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(16.0),
-                            topRight: Radius.circular(16.0),
-                          ),
-                          color: Color(0xff344955),
-                        ),
-                        child: Stack(
-                          alignment: Alignment(0, 0),
-                          overflow: Overflow.visible,
-                          children: <Widget>[
-                            Positioned(
-                              top: -36,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(50)),
-                                    border: Border.all(
-                                        color: Color(0xff232f34), width: 10)),
-                                child: Center(
-                                  child: ClipOval(
-                                    child: Image.asset(
-                                      "assets/images/HA/yang2.jpg",
-                                      fit: BoxFit.cover,
-                                      height: 36,
-                                      width: 36,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              child: ListView.builder(
-                                  physics: BouncingScrollPhysics(),
-                                  itemCount: temp,
-                                  itemBuilder: (BuildContext context, int i) {
-                                    return ListTile(
-                                      leading: Icon(Icons.people),
-                                      title: Text(
-                                          "${_eventss[DateTime.parse(day.toString().replaceAll("12", "00").replaceAll("Z", ""))][i]}"),
-                                      trailing: Icon(Icons.ac_unit),
-                                      onTap: () {
-                                        print(day
-                                            .toString()
-                                            .replaceAll("12", "00")
-                                            .replaceAll("Z", ""));
-                                      },
-                                    );
-                                  }),
-                            )
-                          ],
-                        ))),
-                Container(
-                  height: 56,
-                  color: Color(0xff4a6572),
-                )
-              ],
-            ),
-          );
-        });
-  }
-
-  // More advanced TableCalendar configuration (using Builders & Styles)
-  Widget _buildTableCalendarWithBuilders() {
-    return TableCalendar(
-      rowHeight: 70,
-      locale: 'ko_KO',
-      calendarController: _calendarController,
-      events: _events,
-      holidays: _holidays,
-      initialCalendarFormat: CalendarFormat.month,
-      formatAnimation: FormatAnimation.slide,
-      startingDayOfWeek: StartingDayOfWeek.sunday,
-      availableGestures: AvailableGestures.all,
-      availableCalendarFormats: const {
-        CalendarFormat.month: '',
-        // CalendarFormat.twoWeeks: '',
-        // CalendarFormat.week: '',
-      },
-      calendarStyle: CalendarStyle(
-        outsideDaysVisible: true,
-        weekendStyle: TextStyle().copyWith(color: Colors.blue[800]),
-        holidayStyle: TextStyle().copyWith(color: Colors.blue[800]),
-      ),
-      daysOfWeekStyle: DaysOfWeekStyle(
-        weekendStyle: TextStyle().copyWith(color: Colors.blue[600]),
-      ),
-      headerStyle: HeaderStyle(
-        centerHeaderTitle: true,
-        formatButtonVisible: false,
-      ),
-      builders: CalendarBuilders(
-        selectedDayBuilder: (context, date, _) {
-          if (_calendarController.isToday(date)) {
-            return Container(
-              alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              child: Text(
-                '${date.day}',
-                style: TextStyle(color: Colors.white).copyWith(fontSize: 16.0),
-              ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(200)),
-                  color: Colors.redAccent),
-            );
-          } else {
-            return FadeTransition(
-              opacity:
-                  Tween(begin: 0.0, end: 1.0).animate(_animationController),
-              child: Container(
-                // alignment: Alignment.center,
-                padding: const EdgeInsets.only(top: 5),
-                alignment: Alignment.topCenter,
-                // color: Colors.deepOrange[300],
-                width: 100,
-                height: 100,
-                child: Text(
-                  '${date.day}',
-                  style: TextStyle().copyWith(fontSize: 16.0),
-                ),
-              ),
-            );
-          }
-        },
-        todayDayBuilder: (context, date, _) {
-          return Container(
-            alignment: Alignment.center,
-            // color: Colors.amber[400],
-            width: 100,
-            height: 100,
-            child: Text(
-              '${date.day}',
-              style: TextStyle(color: Colors.white).copyWith(fontSize: 16.0),
-            ),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(200)),
-                color: Colors.redAccent),
-          );
-        },
-        dayBuilder: (context, date, _) {
-          return Container(
-            padding: const EdgeInsets.only(top: 5),
-            alignment: Alignment.topCenter,
-            width: 100,
-            height: 100,
-            child: Text(
-              '${date.day}',
-              style: TextStyle().copyWith(fontSize: 16.0),
-            ),
-            decoration: BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(color: Colors.grey[900], width: 0.2))),
-          );
-        },
-        markersBuilder: (context, date, events, holidays) {
-          final children = <Widget>[];
-
-          if (events.isNotEmpty) {
-            children.add(
-              Positioned(
-                right: 5,
-                bottom: 5,
-                child: _buildEventsMarker(date, events),
-              ),
-            );
-          }
-
-          if (holidays.isNotEmpty) {
-            children.add(
-              Positioned(
-                left: 5,
-                bottom: 5,
-                child: _buildHolidaysMarker(date, holidays),
-              ),
-            );
-          }
-
-          return children;
-        },
-      ),
-      onDaySelected: (date, events, holidays) {
-        _onDaySelected(date, events, holidays);
-        _animationController.forward(from: 0.0);
-      },
-      onVisibleDaysChanged: _onVisibleDaysChanged,
-      onCalendarCreated: _onCalendarCreated,
-    );
-  }
-
-  Widget _buildEventsMarker(DateTime date, List events) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.cyan[50]),
-      width: 20.0,
-      height: 20.0,
-      child: Center(
-        child: Text(
-          '${events.length}',
-          style: TextStyle().copyWith(
-            color: Colors.cyan[900],
-            fontSize: 12.0,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildHolidaysMarker(DateTime date, List holiday) {
-    // return Icon(
-    //   Icons.add_box,
-    //   size: 20.0,
-    //   color: Colors.blueGrey[800],
-    // );
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.blue[400],
-      ),
-      width: 20.0,
-      height: 20.0,
-      child: Center(
-        child: Text(
-          '${holiday.length}',
-          style: TextStyle().copyWith(
-            color: Colors.white,
-            fontSize: 12.0,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildEventList() {
-    return ListView(
-      children: _selectedEvents
-          .map((event) => Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 0.8),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                child: ListTile(
-                  title: Text(event.toString()),
-                  onTap: () => print('$event tapped!'),
-                ),
-              ))
-          .toList(),
-    );
-  }
 }
