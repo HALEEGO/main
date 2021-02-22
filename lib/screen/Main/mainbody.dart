54feba40a5c0dc6fb4eead227ce9c89851dc1059
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-02-22 16:27:46 +0900

    Revert "Delete mainbody.dart"
    This reverts commit bdb041c929fee4254915f7fae5237577ab6c14a8.

---
+++ lib/screen/Main/mainbody.dart
@@ -0,0 +1,204 @@
+import 'package:calendar/constants.dart';
+import 'package:calendar/screen/AddCalendar/addCalendar.dart';
+import 'package:calendar/screen/Calendar/calendar.dart';
+import 'package:calendar/screen/Friend/Friend_screen.dart';
+import 'package:calendar/screen/Home/screen/changename.dart';
+import 'package:calendar/screen/Home/screen/userInfo_screen.dart';
+import 'package:calendar/screen/Welcome/welcome_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../Home/home_screen.dart';
+
+class MainBody extends StatefulWidget {
+  final String id;
+  MainBody({this.id});
+
+  @override
+  _MainBodyState createState() => _MainBodyState();
+}
+
+class _MainBodyState extends State<MainBody> {
+  static final storage = FlutterSecureStorage();
+  String userInfo;
+  String id = '';
+
+  @override
+  void initState() {
+    super.initState();
+    id = widget.id;
+    print(id);
+    firstPage();
+    secondPage();
+    thirdPage(); // first페이지로 id전달
+  }
+
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: (_selectedIndex != 1)
+          ? AppBar(
+              backgroundColor: Colors.white,
+              leading: Icon(
+                Icons.calendar_today_sharp,
+                color: kPrimaryColor,
+              ),
+              title: Text(
+                '${widget.id}의' + '\n' + 'SNS CALENDAR',
+                style: TextStyle(color: kPrimaryColor),
+              ),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    Icons.add_circle_outline,
+                    color: kPrimaryColor,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return AddCalendar(
+                            title: "일정 추가하기",
+                            id: id,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                    icon: Icon(
+                      Icons.person_pin_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UserInfo(id: id)));
+                      //내정보 확인할 수 있는 페이지로
+                    }),
+                Padding(padding: EdgeInsets.only(left: 10)),
+              ],
+            )
+          : AppBar(
+              backgroundColor: Colors.white,
+              leading: Icon(
+                Icons.calendar_today_sharp,
+                color: kPrimaryColor,
+              ),
+              title: Text(
+                '${widget.id}의' + '\n' + '친구',
+                style: TextStyle(color: kPrimaryColor),
+              ),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    Icons.add_circle_outline,
+                    color: kPrimaryColor,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return AddCalendar(
+                            title: "일정 추가하기",
+                            id: id,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                    icon: Icon(
+                      Icons.person_pin_outlined,
+                      color: kPrimaryColor,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UserInfo(id: id)));
+                      //내정보 확인할 수 있는 페이지로
+                    }),
+                Padding(padding: EdgeInsets.only(left: 10)),
+              ],
+            ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.grey,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(.60),
+        selectedFontSize: 14,
+        unselectedFontSize: 10,
+        currentIndex: _selectedIndex, //현재 선택된 Index
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            title: Text('HOME'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text('FRIEND'),
+            icon: Icon(Icons.people),
+          ),
+          BottomNavigationBarItem(
+            title: Text('CALENDAR'),
+            icon: Icon(Icons.calendar_today),
+          ),
+          BottomNavigationBarItem(
+            title: Text('ALARM'),
+            icon: Icon(Icons.alarm),
+          ),
+          BottomNavigationBarItem(
+            title: Text('SETTING'),
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+    );
+  }
+
+  List _widgetOptions = [
+    //여기에 페이지 넣으면됨
+    null,
+    null,
+    null,
+    Text(
+      'News',
+      style: TextStyle(
+        fontSize: 30,
+      ),
+    ),
+    Text(
+      'News',
+      style: TextStyle(
+        fontSize: 30,
+      ),
+    ),
+  ];
+  void firstPage() {
+    _widgetOptions[0] = HomeScreen(
+      id: id,
+    );
+  }
+
+  void secondPage() {
+    _widgetOptions[1] = Friendscreen(id: id);
+  }
+
+  void thirdPage() {
+    _widgetOptions[2] = Calendar(title: id);
+  }
+}
