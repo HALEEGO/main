a9e8a77310b28ce3a9066669253037bd3070e7e2
sfseed3y <78024945+sfseed3y@users.noreply.github.com>
2021-02-24 17:51:47 +0900

    feat: kNoticeColor
    Ok함수에 필요한 색상을 지정

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Colors.white10;
 const kPrimaryLightColor = Colors.white30;
+const kNoticeColor = Colors.blueGrey; // kNoticeColor 부른 후 [800] 넣어주기
 
 // 테마 주색상을 보라, 테마 보조색상을 하양으로 설정
 // HA 2020-02-01
