5c51840c28030ee9329c38c88978abff81172a34
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-02-26 02:01:09 +0900

    feat: addFriend

--- lib/screen/Friend/addFriend.dart
+++ lib/screen/Friend/addFriend.dart
@@ -3,10 +3,8 @@ import 'package:http/http.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar/components/text_field_container.dart';
 import '../../data/User.dart';
-import 'package:flutter_swipe_action_cell/core/cell.dart';
 import 'dart:ui';
 import 'package:calendar/components/rounded_button.dart';
-import './Friend_screen.dart';
 
 class RoundedFriendField extends StatelessWidget {
   final ValueChanged<String> onChanged;
@@ -20,19 +18,17 @@ class RoundedFriendField extends StatelessWidget {
     return TextFieldContainer(
         child: TextField(
       style: TextStyle(color: Colors.white),
-      obscureText: true,
       onChanged: onChanged,
       decoration: InputDecoration(
         hintStyle: TextStyle(color: Colors.white, fontSize: 15),
         hintText: "친구 ID",
-        icon: Icon(Icons.lock, color: Colors.white),
+        icon: Icon(Icons.arrow_forward_ios_rounded, color: Colors.white),
       ),
     ));
   }
 }
 
 class AddFriend extends StatefulWidget {
-  //scheduleTYPE
   final String id;
   AddFriend({@required this.id});
 
@@ -42,6 +38,9 @@ class AddFriend extends StatefulWidget {
 
 class _AddFriendState extends State<AddFriend> {
   String id;
+  Future<User> fetchedFriendID;
+  Future<List<User>> checkFriend;
+  bool buttonOn = false;
   @override
   void initState() {
     super.initState();
@@ -51,17 +50,33 @@ class _AddFriendState extends State<AddFriend> {
   @override
   Widget build(BuildContext context) {
     String _friendID;
-    Future<User> fetchedFriendID;
+    Size size = MediaQuery.of(context).size;
+
+    Future<List<User>> fetchFriend() async {
+      final String url = "http://3.35.39.202:8000/calendar";
+      final response = await get("$url/read/userfriend/${widget.id}");
+      if (response.statusCode == 200) {
+        var responseJson = (json.decode(response.body));
+        return (responseJson as List).map((j) => User.fromJson(j)).toList();
+      } else {
+        throw Exception('Failed to load friendList');
+      }
+    }
+
     Future<User> fetchAddFriend() async {
       final String url = "http://3.35.39.202:8000/calendar";
       final response = await get("$url/read/user/$_friendID");
       if (response.statusCode == 200) {
-        var responseJson = (json.decode(response.body));
-        return (responseJson);
+        Map<String, dynamic> userMap = json.decode(response.body);
+        User user = User.fromJson(userMap);
+        print("$user");
+        return (user);
       } else {
         throw Exception('Failed to load addfriend');
       }
     }
+
+    bool isAlreadyFriend(String friend) {}
 
     void showAlertDialog(BuildContext context, String tmp, String tmp2,
         bool istwo, String friend) async {
@@ -75,67 +90,56 @@ class _AddFriendState extends State<AddFriend> {
             actions: istwo
                 ? <Widget>[
                     FlatButton(
-                      child: Text('마즘'),
-                      onPressed: () async {
-                        Response response = await post(
-                            "http://3.35.39.202:8000/calendar/insert/friend/$id/$friend");
-                        (response.body == 'ok')
-                            ? showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  Widget okButton = FlatButton(
-                                    child: Text("OK"),
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  Friendscreen()));
-                                    },
-                                  );
-                                  return AlertDialog(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(8.0)),
-                                    title: new Text("SIGNUP COMPLETE"),
-                                    content: SingleChildScrollView(
-                                        child:
-                                            new Text("return FRIEND SCREEN")),
-                                    actions: [okButton],
-                                  );
-                                },
-                              )
-                            : showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  Widget okButton = FlatButton(
-                                    child: Text("OK"),
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  Friendscreen()));
-                                    },
-                                  );
-                                  return AlertDialog(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(8.0)),
-                                    title: new Text("SIGNUP FALSE"),
-                                    content: SingleChildScrollView(
-                                        child:
-                                            new Text("return FRIEND SCREEN")),
-                                    actions: [okButton],
-                                  );
-                                },
-                              );
-                      },
-                    ),
+                        child: Text('확인'),
+                        onPressed: () async {
+                          checkFriend = fetchFriend();
+                          bool check = false;
+                          bool isSend = false;
+                          checkFriend.then((val) async {
+                            for (int i = 0; i < val.length; i++) {
+                              (val[i].getUserID == friend)
+                                  ? check = true
+                                  : check = false;
+                            }
+                            if (check) {
+                              showDialog(
+                                  context: context,
+                                  builder: (builder) {
+                                    return AlertDialog(
+                                      title: Text('이미 추가된 친구입니다.'),
+                                      actions: [
+                                        FlatButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text('확인'))
+                                      ],
+                                    );
+                                  });
+                            } else {
+                              Response response = await post(
+                                  "http://3.35.39.202:8000/calendar/insert/friend/$id/$friend");
+                              showDialog(
+                                  context: context,
+                                  builder: (builder) {
+                                    return AlertDialog(
+                                      title: Text('친구 추가 완료'),
+                                      actions: [
+                                        FlatButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text('확인'))
+                                      ],
+                                    );
+                                  });
+                            }
+                          });
+                        }),
                     FlatButton(
-                      child: Text('틀림'),
+                      child: Text('취소'),
                       onPressed: () {
-                        Navigator.pop(context, "OK");
+                        Navigator.pop(context);
                       },
                     ),
                   ]
@@ -159,32 +163,17 @@ class _AddFriendState extends State<AddFriend> {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
         leading: IconButton(
-            icon: Icon(
-              Icons.keyboard_backspace_rounded,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        backgroundColor: Colors.blueGrey[800],
-                        title: new Text(
-                          "오잉",
-                          style: TextStyle(color: Colors.white70),
-                        ),
-                        content: SingleChildScrollView(
-                            child: new Text(
-                          "ㅋㅋ",
-                          style: TextStyle(color: Colors.white70),
-                        )),
-                      ));
-            }),
-        title: Text("친추하기"),
+          icon: Icon(
+            Icons.keyboard_backspace_rounded,
+            color: Colors.white70,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.black,
+        title: Text("친구 추가하기", style: TextStyle(color: Colors.white70)),
       ),
       body: Container(
         child: Column(
@@ -194,57 +183,92 @@ class _AddFriendState extends State<AddFriend> {
                 RoundedFriendField(onChanged: (value) {
                   _friendID = value;
                 }),
+                Container(
+                  width: size.width * 0.2,
+                ),
                 IconButton(
                     icon: Icon(
                       Icons.search,
                       color: Colors.white,
                     ),
                     onPressed: () {
-                      setState(() {
-                        fetchedFriendID = fetchAddFriend();
-                        Container(
-                            child: FutureBuilder<User>(
-                                future: fetchedFriendID,
-                                builder: (context, snapshot) {
-                                  return (snapshot.data.getUserIDK == 0
-                                      ? Text("아이디 제대로 확인하세요 ^^7",
-                                          style: TextStyle(color: Colors.white))
-                                      : Text(
-                                          "${snapshot.data.getUserNAME}, ${snapshot.data.getUserID}"));
-                                }));
-                        Container(
-                            child: FutureBuilder<User>(
-                                future: fetchedFriendID,
-                                builder: (context, snapshot) {
-                                  return RoundedButton(
-                                    text: "친구추가",
-                                    press: () {
-                                      if (snapshot.data == null ||
-                                          snapshot.data.getUserIDK == 0) {
-                                        showAlertDialog(
-                                            context,
-                                            "친구 아이디 오류",
-                                            "다시 해바",
-                                            false,
-                                            snapshot.data.getUserID);
-                                      } else {
-                                        showAlertDialog(
-                                            context,
-                                            "진짜 친추?",
-                                            "${snapshot.data.getUserNAME} 이름 맞제?",
-                                            true,
-                                            snapshot.data.getUserID);
-                                      }
-                                    },
-                                  );
-                                }));
-                      });
+                      setState(
+                        () {
+                          fetchedFriendID = fetchAddFriend();
+                          buttonOn = true;
+                        },
+                      );
                     })
               ]),
             ),
+            Container(height: size.height * 0.1),
+            (buttonOn == false)
+                ? Container(
+                    child: Text("추가할 친구의 ID를 입력하세요.",
+                        style: TextStyle(color: Colors.white70, fontSize: 20)))
+                : Container(
+                    child: FutureBuilder<User>(
+                        future: fetchedFriendID,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ((snapshot.data.getUserIDK == 0)
+                                ? Text("입력한 ID에 해당하는 친구가 존재하지 않습니다.",
+                                    style: TextStyle(
+                                        color: Colors.white70, fontSize: 15))
+                                : Column(
+                                    children: [
+                                      Text(
+                                          "친구 아이디: ${snapshot.data.getUserID}   친구 이름: ${snapshot.data.getUserNAME}",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18)),
+                                      Container(height: size.height * 0.05),
+                                      RoundedButton(
+                                          text: "친구 추가",
+                                          press: () {
+                                            showAlertDialog(
+                                                context,
+                                                "추가하시겠습니까?",
+                                                "친구 이름: ${snapshot.data.getUserNAME}",
+                                                true,
+                                                snapshot.data.getUserID);
+                                          })
+                                    ],
+                                  ));
+                          }
+                          return CircularProgressIndicator();
+                        })),
           ],
         ),
       ),
     );
   }
 }
+
+// leading: IconButton(
+//             icon: Icon(
+//               Icons.keyboard_backspace_rounded,
+//               color: Colors.white70,
+//             ),
+//             onPressed: () {
+//               showDialog(
+//                   context: context,
+//                   builder: (context) => AlertDialog(
+//                         shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(8.0),
+//                         ),
+//                         backgroundColor: Colors.blueGrey[800],
+//                         title: new Text(
+//                           "경고",
+//                           style: TextStyle(color: Colors.white70),
+//                         ),
+//                         content: SingleChildScrollView(
+//                             child: new Text(
+//                           "뒤로가려면 뒤로가기를 누르세요",
+//                           style: TextStyle(color: Colors.white70),
+//                         )),
+//                       ));
+//             }),
+
+
+
