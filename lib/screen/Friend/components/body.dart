740079136d1eb0b81a2c6ace5e4636e38a5a462d
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2021-02-26 15:24:17 +0900

    feat: 친구삭제 구현

--- lib/screen/Friend/components/body.dart
+++ lib/screen/Friend/components/body.dart
@@ -142,11 +142,17 @@ class _BodyState extends State<Body> {
                               itemCount: snapshot.data.length,
                               itemBuilder: (BuildContext context, int i) {
                                 return ListTile(
-                                  leading: Icon(Icons.emoji_people),
+                                  leading: Icon(
+                                    Icons.emoji_people,
+                                    color: Colors.white,
+                                  ),
                                   title: Text("${snapshot.data[i].getUserNAME}",
                                       style: TextStyle(color: Colors.white)),
                                   trailing: DropdownButton<String>(
-                                    icon: Icon(Icons.more_vert_outlined),
+                                    icon: Icon(
+                                      Icons.more_vert_outlined,
+                                      color: Colors.white,
+                                    ),
                                     iconSize: 24,
                                     underline: Container(
                                       color: Colors.white,
@@ -159,34 +165,62 @@ class _BodyState extends State<Body> {
                                       setState(() {
                                         dropdownValue = newValue;
                                       });
-                                      (dropdownValue == '상세정보')
-                                          ? showDialog(
-                                              context: context,
-                                              builder: (BuildContext context) {
-                                                return AlertDialog(
-                                                  shape: RoundedRectangleBorder(
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              8.0)),
-                                                  title: new Text(
-                                                      "${snapshot.data[i].getUserNAME} 정보"),
-                                                  content: SingleChildScrollView(
-                                                      child: new Text(
-                                                          "이름: ${snapshot.data[i].getUserNAME}, 아이디: ${snapshot.data[i].getUserID}")),
-                                                );
-                                              },
-                                            )
-                                          : Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      Calendar(
-                                                          title: snapshot
-                                                              .data[i]
-                                                              .getUserID)),
+                                      if (dropdownValue == '상세정보') {
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          8.0)),
+                                              title: new Text(
+                                                  "${snapshot.data[i].getUserNAME} 정보"),
+                                              content: SingleChildScrollView(
+                                                  child: new Text(
+                                                      "이름: ${snapshot.data[i].getUserNAME}, 아이디: ${snapshot.data[i].getUserID}")),
                                             );
+                                          },
+                                        );
+                                      } else if (dropdownValue == '달력보기') {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => Calendar(
+                                                  title: snapshot
+                                                      .data[i].getUserID)),
+                                        );
+                                      } else {
+                                        showDialog(
+                                            context: context,
+                                            builder: (builder) {
+                                              return AlertDialog(
+                                                title: Text("삭제하시겠습니까?"),
+                                                content: Text(
+                                                    "${snapshot.data[i].getUserNAME}을 정말 삭제하시겠습니까?"),
+                                                actions: [
+                                                  FlatButton(
+                                                      onPressed: () async {
+                                                        Response response =
+                                                            await delete(
+                                                                "http://3.35.39.202:8000/calendar/delete/friend/$id/${snapshot.data[i].getUserID}");
+                                                        Navigator.pop(context,
+                                                            setState(() {
+                                                          id = widget.id;
+                                                        }));
+                                                      },
+                                                      child: Text("확인")),
+                                                  FlatButton(
+                                                      onPressed: () {
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: Text("취소"))
+                                                ],
+                                              );
+                                            });
+                                      }
                                     },
-                                    items: <String>['상세정보', '달력보기']
+                                    items: <String>['상세정보', '달력보기', '친구삭제']
                                         .map<DropdownMenuItem<String>>(
                                             (String value) {
                                       return DropdownMenuItem<String>(
